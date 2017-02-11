79a082e77c48cfc8064334374564845fc4f5ad5e
Ivan Todorov <Ivan Todorov>
2017-02-11 23:47:39 +0200

    First commit

---
+++ ChapterOne.java
@@ -0,0 +1,11 @@
+
+public class ChapterOne {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1 2 3 4 5");
+		System.out.println("1 2 3 4 5");
+		System.out.println("1 2 3 4 5");
+		
+	}
+}
